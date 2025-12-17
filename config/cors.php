f9781c0a5813c5073ecb920fbbaf6cd295c914a2
dzikriii24 <dzikrirabbani2401@gmail.com>
2025-12-17 20:44:11 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,24 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'https://gamestoredz.vercel.app')],
+    
+    'allowed_origins' => [
+        env('APP_URL', 'http://localhost'),
+        env('FRONTEND_URL', 'https://gamestoredz.vercel.app'),
+        'http://localhost:5173',
+        'http://localhost:3000'
+    ],
+    
     'allowed_origins_patterns' => [],
+    
     'allowed_headers' => ['*'],
+    
     'exposed_headers' => [],
+    
     'max_age' => 0,
+    
     'supports_credentials' => false,
 ];
